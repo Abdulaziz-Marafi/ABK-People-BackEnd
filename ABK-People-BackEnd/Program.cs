f133c3267edc3e2a77c6970f2a3bb47ee6261ceb
Abdulaziz-Marafi <azizbm4798@gmail.com>
2025-03-16 19:32:23 +0300

    cors setup

--- ABK-People-BackEnd/Program.cs
+++ ABK-People-BackEnd/Program.cs
@@ -41,7 +41,16 @@ builder.Services.AddAuthentication(options =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy => policy.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+});
+
 var app = builder.Build();
+app.UseCors("AllowAll");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
